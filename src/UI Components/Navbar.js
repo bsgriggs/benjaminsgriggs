8b554e804fc6e0d4ae5a6e04bd535052ed653d09
bsgriggs <usbeanie@gmail.com>
2019-12-14 00:49:14 -0500

    Cleanup

--- src/UI Components/Navbar.js
+++ src/UI Components/Navbar.js
@@ -7,15 +7,15 @@ export class Navbar extends React.Component{
 		super(props);
 
 		//other methods
-		this.slideNav = this.slideNav.bind(this);
+		// this.slideNav = this.slideNav.bind(this);
 		this.clickLogo = this.clickLogo.bind(this);
 	}
 
-	slideNav(){
-		const slid = document.getElementById("slider");
-		slid.classList.toggle("down");
-		slid.classList.toggle("up");
-	}
+	// slideNav(){
+	// 	const slid = document.getElementById("slider");
+	// 	slid.classList.toggle("down");
+	// 	slid.classList.toggle("up");
+	// }
 
 	clickLogo(){
 		window.open("/","_self")
@@ -25,35 +25,33 @@ export class Navbar extends React.Component{
   	render() {
 		const FirstNavActive = () => {
 			const sub = this.props.location.pathname.substring(0,2);
-			if (sub ==='/a' || sub === '/f' || sub === '/')
+			if (sub === '/')
 				return true;
 			return false;
 		};
 
 		return (
 			<div>
-				{/* <div id="slider" className="up">
+				<div id="slider" className="up">
 					<div className="Slider" onMouseEnter={this.slideNav} onMouseLeave={this.slideNav}>
-						<NavLink exact to='/'>Benjamin</NavLink>
-						<NavLink to='/feedback'>Feedback</NavLink>
+						<NavLink exact to='/'>Home</NavLink>
+						<NavLink to='/travel'>Travel</NavLink>
+						<a href='./Benjamin_Griggs_Resume.pdf' target="_blank" >Resume</a>
 					</div>
-				</div> */}
+				</div>
 
 				<div className="Navbar">
-					<img src={"/imgs/BenjaminSGriggs.svg"} alt="logo" onClick={this.clickLogo}/>
-					<NavLink id='home' to='/' 
-						// onMouseEnter={this.slideNav} 
-						// onMouseLeave={this.slideNav} 
-						isActive={FirstNavActive}
-						>Home
-							{/* <i className="fas fa-caret-down"  ></i>  */}
-					</NavLink>
-					{/* <NavLink to='/minecraft'>Minecraft</NavLink> */}
-					<NavLink to='/skills'>Skills</NavLink>
-					<NavLink to='/projects'>Projects</NavLink>
-					<a href='./Benjamin_Griggs_Resume.pdf' target="_blank" >Resume</a>
-					<NavLink to='/travel'>Travel</NavLink>
-					<NavLink to='/contact'>Contact</NavLink>
+					<div id='first'>
+						<img src={"/imgs/BenjaminSGriggs.svg"} alt="logo" onClick={this.clickLogo}/>
+						<NavLink id='home' to='/' isActive={FirstNavActive}>Home</NavLink>
+						<NavLink to='/skills'>Skills</NavLink>
+						<NavLink to='/projects'>Projects</NavLink>
+						<NavLink id='travel' to='/travel'>Travel</NavLink>
+					</div>
+					<div id='second'>
+						<a id='resume' href='./Benjamin_Griggs_Resume.pdf' target="_blank" >Resume</a>
+						<NavLink to='/contact'>Contact</NavLink>
+					</div>
 				</div>
 			</div>
 		);
--- src/UI Components/Projects.js
+++ src/UI Components/Projects.js
@@ -1,41 +1,6 @@
 import React from 'react'
 import Project from './Project'
 import projectRoot from '../projects.json'
-// import {ImageSlides} from './ImageSlides'
-// //ATP
-// import atp from '../imgs/Screenshots/atpHome.jpg'
-// import atpOrder from '../imgs/Screenshots/atpOrder.jpg'
-// import atpEmail from '../imgs/Screenshots/atpEmail.jpg'
-// import atpQuote from '../imgs/Screenshots/atpQuoteLDAP.jpg'
-// import atpDash from '../imgs/Screenshots/atpDashboard.jpg'
-// //CPM
-// import cpm from '../imgs/Screenshots/cpmHome.jpg'
-// import cpmSearch from '../imgs/Screenshots/cpmSearch.jpg'
-// import cpmJob from '../imgs/Screenshots/cpmJob.jpg'
-// import cpmScheduledEvent from '../imgs/Screenshots/cpmScheduledEvent.jpg'
-// //benjaminsgriggs
-// import benjaminsgriggs from '../imgs/Screenshots/benjaminsgriggs.jpg'
-// //cost
-// import cost from '../imgs/Screenshots/costHome.jpg'
-// import costOverview from '../imgs/Screenshots/costOverview.jpg'
-// import costReport from '../imgs/Screenshots/costReport.jpg'
-// //tetris
-// import tetris from '../imgs/Screenshots/tetris.jpg'
-
-// import mendix from '../imgs/mendix.jpg'
-// import java from '../imgs/java.jpg'
-// import c from '../imgs/c.jpg'
-// import css from '../imgs/css.jpg'
-// import html from '../imgs/html.jpg'
-// import javascript from '../imgs/javascript.jpg'
-// import react from '../imgs/React.jpg'
-// import node from '../imgs/nodejs.jpg'
-// import postgresql from '../imgs/PostgreSQL.jpg'
-// import git from '../imgs/github.jpg'
-// import raspberry from '../imgs/raspberry.jpg'
-// import ubuntu from '../imgs/ubuntu.jpg'
-
-import './Projects.css'
 
 class Projects extends React.Component{
 	render() {
